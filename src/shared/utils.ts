597fb3db6c0e3cc7a6f9a06704b105d73c11bebc
Pedro Gutiérrez <pedrogp9693@gmail.com>
2021-09-27 14:15:45 +0200

    Add constants of used categories for DeFi assets and create a way to map them

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -1,3 +1,19 @@
+import { DDO, MetaData } from '@nevermined-io/nevermined-sdk-js'
+
+import { categories, subcategories, networks } from './constants'
+
 export function toDate(date: string) {
   return new Date(date).toLocaleDateString('en-uk')
+}
+
+export function getDefiInfo({additionalInformation}: MetaData) {
+  const cats = additionalInformation?.categories
+  if (cats) {
+    const contains = (list: string[]) => cats.find(cat => list.includes(cat))
+    return {
+      category: contains(categories), 
+      subcategory: contains(subcategories), 
+      network: contains(networks),
+    }
+  }
 }
