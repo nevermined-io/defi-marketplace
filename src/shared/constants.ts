7417a9d508765c277517f7932410ef4fbecd11ce
Pedro Gutiérrez <pedrogp9693@gmail.com>
2021-09-27 14:15:45 +0200

    Add constants of used categories for DeFi assets and create a way to map them

---
+++ src/shared/constants.ts
@@ -0,0 +1,25 @@
+export enum Category {
+  Dex = 'Dex',
+  Lending = 'Lending',
+}
+export const categories = Object.keys(Category)
+
+export enum Subcategory {
+  Trades = 'Trades',
+  Borrows = 'Borrows',
+  Deposits = 'Deposits',
+  Flashloans = 'Flashloans',
+  Liquidations = 'Liquidations',
+  Redeems = 'Redeems',
+  Repays = 'Repays',
+}
+export const subcategories = Object.keys(Subcategory)
+
+export enum Network {
+  Ethereum = 'Ethereum',
+  Polygon = 'Polygon',
+  Arbitrum = 'Arbitrum',
+  Bsc = 'BSC',
+  Fantom = 'Fantom',
+}
+export const networks = Object.keys(Network)
