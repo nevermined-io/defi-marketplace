0edfae13253f0cd1fa5cc96b1982877c3f75138a
obasilakis <vasileios.georgopoulos@gmail.com>
2021-11-11 19:00:38 +0100

    Filter based on publication date

--- src/components/dropdown/dropdown.tsx
+++ src/components/dropdown/dropdown.tsx
@@ -1,24 +1,42 @@
-import React, { HTMLAttributes, Props, useState } from 'react'
+import React, { HTMLAttributes, Props, useEffect, useRef, useState } from 'react'
 import { BEM } from 'ui'
 import Image from "next/image"
 import styles from './dropdown.module.scss'
 
 interface DropdownProps {
+  imgHeight: string
+  imgSrc: string
+  imgWidth: string
+  onClickOutside: Function
+  title: string
 }
 
 const b = BEM('dropdown', styles)
 
 export function UiDropdown(props: DropdownProps & HTMLAttributes<any> & Props<any>) {
-  const { children } = props
+  const { children, imgHeight, imgSrc, title, imgWidth } = props
   const [clicked, setClicked] = useState<boolean>(false)
+  let ref = useRef(null)
 
-  return <div className={b('wrapper')}>
+  const handleClickOutside = (event: any) => {
+    if (ref.current && !ref.current.contains(event.target)) {
+      setClicked(false)
+    }
+  }
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside, true)
+    return () => {
+      document.removeEventListener('click', handleClickOutside, true)
+    }
+  })
+
+  return <div className={b('wrapper')} ref={ref}>
     <div className={b('toggle')} onClick={() => {
       setClicked(!clicked)
     }}>
-      Category
+      {title}
       <div className={b(`${clicked ? 'image-wrapper' : 'image-wrapper--clicked'}`)}>
-        <Image height="6px" width="10px" src="/assets/arrow.svg"/>
+        <Image height={imgHeight} width={imgWidth} src={imgSrc}/>
       </div>
     </div>
     <div className={b('menu')} style={{ display: clicked ? 'block' : 'none' }}>
