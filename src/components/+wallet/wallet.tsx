f837303eb850b88dff8f1c5185bf620fafab8c08
Fabriziogianni7 <fabrizio@keyko.io>
2021-11-16 16:38:46 +0100

    implemented popup in wallet.ts

--- src/components/+wallet/wallet.tsx
+++ src/components/+wallet/wallet.tsx
@@ -1,5 +1,5 @@
-import React, { Props } from 'react'
-import { BEM, modList, extendClassName, UiButton } from 'ui'
+import React, { Props, createRef, useEffect, useState } from 'react'
+import { BEM, modList, extendClassName, UiButton, UiPopup, UiPopupHandlers } from 'ui'
 import { User } from '../../context'
 import styles from './wallet.module.scss'
 
@@ -9,21 +9,42 @@ interface WalletProps {
 const b = BEM('wallet', styles)
 
 export function XuiWallet(props: WalletProps) {
-  const {isLogged, account, network, loginMetamask} = React.useContext(User)
+  const { isLogged, account, network, loginMetamask, switchToMumbai } = React.useContext(User)
+  const [connected, connect] = useState(false)
+
+  const UiRef = createRef<UiPopupHandlers>()
+
+  useEffect(() => {
+    network && network !== 'mumbai' ?
+      UiRef.current?.open() : connected ?
+        UiRef.current?.close() : null
+  }, [UiRef, network, connected]);
+
+  const handleChangeNetwork = () => {
+    switchToMumbai()
+    connect(true)
+  }
+
   return !(isLogged && account)
     ? (
       <UiButton onClick={loginMetamask}>
         Connect wallet
       </UiButton>
-    ) : (
+    ) :
+    (
       <>
         <div className={b('block')}>
-          <span className={b('logged')}/>
+          <span className={b('logged')} />
           {`${account.substr(0, 6)}...${account.substr(-4)}`}
         </div>
         <div className={b('block', ['network'])}>
           {network}
         </div>
+        <UiPopup ref={UiRef}>
+          <UiButton onClick={handleChangeNetwork}>
+            Switch To Mumbai Network
+          </UiButton>
+        </UiPopup>
       </>
     )
 }
