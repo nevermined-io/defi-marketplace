c6d90cf1f3c7c3c3677c1897d5443c4a0b3e6493
Ialberquilla <ivan.alberquilla@gmail.com>
2021-12-15 12:47:25 +0100

    server

---
+++ server/src/index.ts
@@ -0,0 +1,51 @@
+import express from 'express'
+import morgan from 'morgan'
+import bodyParser from 'body-parser'
+import pkg from '../package.json'
+import compression from 'compression'
+import config from './config'
+import db from './models'
+
+import BundleRouter from './routes/BundlesRouter'
+
+const app = express()
+
+app.use((_, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  )
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, DELETE'
+  )
+  next()
+})
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(compression())
+
+app.get('/', (_, res) => {
+  res.send(
+    `<strong><code>
+          Nevermined DeFi Marketplace Server v${pkg.version}<br />
+          <a href="https://github.com/nevermined-io/defi-marketplace"
+          style="text-decoration:none;color:#190078">https://github.com/nevermined-io/defi-marketplace</a>
+      </code></strong>`
+  )
+})
+
+app.use('/api/v1/bundle', BundleRouter)
+
+app.use((_, res) => {
+  res.status(404).send()
+})
+
+  db.sequelize.authenticate().then(
+    app.listen(config.app.port, () => {
+      console.log(`Sever listening`)
+    })
+  ).catch(err => console.log(`Error connecting with the db ${err}`))
+
---
+++ server/src/models/Dataset.ts
@@ -0,0 +1,41 @@
+import { Model } from "sequelize";
+import { Table } from 'sequelize-typescript'
+
+export interface IDataset {
+  dataset_id: number,
+  key: string,
+  source: string
+}
+
+module.exports = (sequelize: any, DataTypes: any) => {
+  @Table({ tableName: "datasets" })
+  class Dataset extends Model<IDataset> {
+    dataset_id: number;
+    key: string;
+    source: string;
+
+    static associate(models: any) {
+      Dataset.belongsToMany(models.Bundles, {
+        through: 'dataset_bundles'
+      })
+    }
+  }
+
+  Dataset.init({
+    dataset_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true,
+    },
+    key: DataTypes.STRING,
+    source: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Datasets',
+    timestamps: false
+  })
+
+  return Dataset
+
+}
+
