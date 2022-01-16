2513591cffe371eaa0306637c0df679ecdaf9077
obasilakis <vasileios.georgopoulos@gmail.com>
2022-01-16 12:46:39 +0200

    Batch select UI

--- client/src/+assets/assets-list.tsx
+++ client/src/+assets/assets-list.tsx
@@ -1,4 +1,4 @@
-import React, { Props } from 'react'
+import React, { Fragment, useState } from 'react'
 import { DDO } from '@nevermined-io/nevermined-sdk-js'
 import Link from "next/link"
 
@@ -12,13 +12,38 @@ interface AssetsListProps {
 
 const b = BEM('assets-list', styles)
 export function AssetsList({assets}: AssetsListProps) {
+  const [batchActive, setBatchActive] = useState<boolean>(false)
+
   return (
     <div className={b()}>
+      <div className={b('heading')}>
+        <div className={b('batch-select-wrapper')}>
+          {batchActive ?
+            <Fragment>
+              <div className={b('batch-select')}>
+                <img className={b('batch-checkbox')} src={'assets/unchecked_box.svg'} width="14px" />
+                <div className={b('selected-count')}>Selected: 0</div>
+                <img
+                  className={b('batch-close')}
+                  onClick={() => setBatchActive(false)}
+                  src={'assets/close.svg'}
+                  width="12px"
+                />
+              </div>
+              <div className={b('basket-add')}>Add to basket</div>
+            </Fragment> :
+            <div className={b('batch-select-inactive')} onClick={() => setBatchActive(true)}>Batch Select</div>
+          }
+        </div>
+      </div>
       {assets
         .map(asset => ({asset, metadata: asset.findServiceByType('metadata').attributes}))
         .map(data => ({...data, defi: getDefiInfo(data.metadata)}))
         .map(({asset, metadata, defi}) => (
           <UiLayout key={asset.id} className={b('asset')}>
+            <div className={b(`${batchActive ? 'checkbox' : 'checkbox--hidden'}`)}>
+              <img src={'assets/unchecked_box.svg'} width="20px" />
+            </div>
             <Link href={`/asset/${asset.id}`}>
               <UiText className={`pointer ${b('asset-title')}`} wrapper="h4" type="h4">{metadata.main.name}</UiText>
             </Link>
@@ -50,8 +75,9 @@ export function AssetsList({assets}: AssetsListProps) {
                 </UiText>
               </UiText>
             </UiLayout>
+            <hr size="40" style={{ border: '1px solid #2B465C', marginRight: '16px' }}/>
             <XuiBuyAsset asset={asset}>
-              <UiIcon icon="download" color="primary" size="l"/>
+              <img width="24px" src="assets/basket_icon.svg"/>
             </XuiBuyAsset>
           </UiLayout>
         ))
