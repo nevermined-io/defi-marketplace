37672819e32ca8823fa3366a2e9ff72117ddcfbd
Fabriziogianni7 <fabrizio@keyko.io>
2022-01-17 10:37:05 +0100

    checkboxes

--- client/src/+assets/assets-list.tsx
+++ client/src/+assets/assets-list.tsx
@@ -1,6 +1,7 @@
-import React, { Props } from 'react'
+import React, { Props, useState } from 'react'
 import { DDO } from '@nevermined-io/nevermined-sdk-js'
 import Link from "next/link"
+
 
 import { BEM, UiLayout, UiText, UiDivider, UiIcon, XuiTokenName, XuiTokenPrice, XuiBuyAsset } from 'ui'
 import { toDate, getDefiInfo, getDdoTokenAddress } from '../shared'
@@ -11,47 +12,65 @@ interface AssetsListProps {
 }
 
 const b = BEM('assets-list', styles)
-export function AssetsList({assets}: AssetsListProps) {
+export function AssetsList({ assets }: AssetsListProps) {
+
+  const [selectedAssets, assetToList] = useState<DDO[]>([])
+
+  const addDelAssetToList = (e: any, asset: any) => {
+    if (e.target.checked){
+      selectedAssets.push(asset)
+      assetToList(selectedAssets)
+      // console.log("selectedAssets", selectedAssets)
+    } else {
+      selectedAssets.splice(selectedAssets.indexOf(asset.id),1)
+      assetToList(selectedAssets)
+      // console.log("selectedAssets", selectedAssets)
+    }
+  }
+
+
   return (
     <div className={b()}>
       {assets
-        .map(asset => ({asset, metadata: asset.findServiceByType('metadata').attributes}))
-        .map(data => ({...data, defi: getDefiInfo(data.metadata)}))
-        .map(({asset, metadata, defi}) => (
+        .map(asset => ({ asset, metadata: asset.findServiceByType('metadata').attributes }))
+        .map(data => ({ ...data, defi: getDefiInfo(data.metadata) }))
+        .map(({ asset, metadata, defi }) => (
           <UiLayout key={asset.id} className={b('asset')}>
+            <input id={asset.id + "-checkbox-id"} type="checkbox" onChange={(e: any) => addDelAssetToList(e, asset)} />
+            <UiDivider vertical />
             <Link href={`/asset/${asset.id}`}>
               <UiText className={`pointer ${b('asset-title')}`} wrapper="h4" type="h4">{metadata.main.name}</UiText>
             </Link>
             <UiText className={b('asset-date')} type="small" variants={['detail']}>
               {toDate(metadata.main.dateCreated).replace(/\//g, '.')}
             </UiText>
-            <UiDivider flex/>
+            <UiDivider flex />
             {defi?.category && defi?.network && (
               <>
                 <UiLayout className={b('info')}>
-                  <UiIcon className={b('info-icon')} icon="folder" color="secondary"/>
+                  <UiIcon className={b('info-icon')} icon="folder" color="secondary" />
                   <UiText variants={['secondary']}>{defi.category}</UiText>
                   <UiText variants={['detail']}>&nbsp;&ndash;&nbsp;</UiText>
                   <UiText variants={['secondary']}>{defi.subcategory}</UiText>
                 </UiLayout>
                 <UiLayout className={b('info')}>
-                  <UiIcon className={b('info-icon')} icon="share" color="secondary"/>
+                  <UiIcon className={b('info-icon')} icon="share" color="secondary" />
                   <UiText variants={['secondary']}>{defi.network}</UiText>
                 </UiLayout>
               </>
             )}
             <UiLayout className={b('info')}>
-              <UiIcon className={b('info-icon')} icon="tag" color="secondary"/>
+              <UiIcon className={b('info-icon')} icon="tag" color="secondary" />
               <UiText variants={['secondary']}>
                 <XuiTokenPrice>{metadata.main.price}</XuiTokenPrice>
                 {' '}
                 <UiText variants={['detail']}>
-                  <XuiTokenName address={getDdoTokenAddress(asset)}/>
+                  <XuiTokenName address={getDdoTokenAddress(asset)} />
                 </UiText>
               </UiText>
             </UiLayout>
             <XuiBuyAsset asset={asset}>
-              <UiIcon icon="download" color="primary" size="l"/>
+              <UiIcon icon="download" color="primary" size="l" />
             </XuiBuyAsset>
           </UiLayout>
         ))
