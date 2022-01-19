8dba83f457da7c82b1d44b0e0be1e8c8dceb85c7
Fabriziogianni7 <fabrizio@keyko.io>
2022-01-19 12:14:46 +0100

    header

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,12 +1,19 @@
 import '../src/styles/globals.scss'
 
-import React from 'react'
+import React, { useEffect } from 'react'
+
+import { useContext } from 'react'
 import Head from 'next/head'
+import Image from "next/image"
+
+
 import type { AppProps } from 'next/app'
+import Router from 'next/router'
 import { UiHeader, UiText, UiButton, UiHeaderLink, UiFooter, UiDivider } from 'ui'
 
 import UserProvider from '../src/context/UserProvider'
 import { User } from '../src/context'
+import Link from 'next/link'
 
 function App({ Component, pageProps }: AppProps) {
   // const context = React.useContext(User)
@@ -22,15 +29,21 @@ function App({ Component, pageProps }: AppProps) {
         <div>
           <UiHeader>
             <UiHeaderLink href="/list">Marketplace</UiHeaderLink>
-            <UiHeaderLink href="/history">History</UiHeaderLink>
-            <UiHeaderLink href="/styles">DEV - Styles</UiHeaderLink>
+            <UiHeaderLink href="/history">Profile</UiHeaderLink>
+            <UiHeaderLink href="/styles">About</UiHeaderLink>
+            <Link href={'/checkout'} >
+              <span style={{ cursor: 'pointer' }}>
+                <Image width="39" height="24" src="/assets/basket_icon.svg" />
+                {basket.length}
+              </span>
+            </Link>
           </UiHeader>
 
           <Component {...pageProps} />
         </div>
 
-        <UiDivider flex/>
-        <UiFooter/>
+        <UiDivider flex />
+        <UiFooter />
       </>
     </UserProvider>
   )
