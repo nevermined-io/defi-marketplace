fb21f5203ee5b0a4cf139a687f808da2b7ee673b
Fabriziogianni7 <fabrizio@keyko.io>
2022-03-01 13:43:16 +0100

    checking wallet and network only on purchase

--- client/src/+assets/checkout.tsx
+++ client/src/+assets/checkout.tsx
@@ -1,21 +1,29 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import type { NextPage } from 'next'
 
-import { UiText, UiLayout, UiDivider, UiIcon, XuiTokenPrice, XuiTokenName, BEM, UiButton, XuiBuyAsset } from 'ui'
+import { UiText, UiLayout, UiDivider, UiIcon, XuiTokenPrice, XuiTokenName, BEM, UiButton} from 'ui'
 import { User } from '../context'
 import { getDdoTokenAddress, getDefiInfo, toDate } from '../shared'
 import Link from 'next/link'
 import styles from './checkout.module.scss'
 import Router from 'next/router'
 import { XuiCreateBundle } from 'ui/+create-bundle/create-bundle'
+import { correctNetworkName } from '../config'
 
 const b = BEM('checkout', styles)
 export const Checkout: NextPage = () => {
-  const { assets: contextAssets, basket, removeFromBasket } = useContext(User)
+  const { assets: contextAssets, basket, network, isLogged, account, removeFromBasket, switchToCorrectNetwork, loginMetamask } = useContext(User)
+  const [connected, connect] = useState(false)
+
   const assets = contextAssets.filter(asset => basket.includes(asset.id))
   const totalPrice = assets.map(
     asset => Number(asset.findServiceByType('metadata').attributes.main.price)
   ).reduce((partialSum, a) => partialSum + a, 0);
+
+  const handleChangeNetwork = () => {
+    switchToCorrectNetwork()
+    connect(true)
+  }
 
   return (
     <>
@@ -24,66 +32,82 @@ export const Checkout: NextPage = () => {
         <UiLayout type="sides" align="start">
           <div className={b('purchases-wrapper')}>
             <UiLayout >
-              <UiText type="h3" wrapper="h2">My purchases</UiText>
+              <UiText type="h3" wrapper="h2">My Shopping Basket</UiText>
             </UiLayout>
-            <hr style={{ borderBottom: '0', borderTop: '1px solid #8AAABE', margin: '22px 0 32px', width: '769px' }}/>
+            <hr style={{ borderBottom: '0', borderTop: '1px solid #8AAABE', margin: '22px 0 32px', width: '769px' }} />
 
             {/* TODO: make assets-list more versatile and reuse it here */}
-            {assets
-              .map(asset => ({asset, metadata: asset.findServiceByType('metadata').attributes}))
-              .map(data => ({...data, defi: getDefiInfo(data.metadata)}))
-              .map(({asset, metadata, defi}) => (
-                <div key={asset.id} className={b('table-wrapper')}>
-                  <UiLayout className={b('asset')}>
-                    <div className={b('checkbox')}>
-                      <img onClick={() => removeFromBasket([asset.id])} src={'assets/close.svg'} width="20px" />
-                    </div>
-                    <Link href={`/asset/${asset.id}`}>
-                      <UiText className={`pointer ${b('asset-title')}`} wrapper="h4" type="h4">{metadata.main.name}</UiText>
-                    </Link>
-                    <UiText className={b('asset-date')} type="small" variants={['detail']}>
-                      {toDate(metadata.main.dateCreated).replace(/\//g, '.')}
-                    </UiText>
-                    <UiDivider flex/>
-                    {defi?.category && defi?.network && (
-                      <>
-                        <UiLayout className={b('info')}>
-                          <UiIcon className={b('info-icon')} icon="folder" color="secondary"/>
-                          <UiText variants={['secondary']}>{defi.category}</UiText>
-                          <UiText variants={['detail']}>&nbsp;&ndash;&nbsp;</UiText>
-                          <UiText variants={['secondary']}>{defi.subcategory}</UiText>
-                        </UiLayout>
-                      </>
-                    )}
-                    <UiLayout className={b('info')}>
-                      <UiIcon className={b('info-icon')} icon="tag" color="secondary"/>
-                      <UiText variants={['secondary']}>
-                        <XuiTokenPrice>{metadata.main.price}</XuiTokenPrice>
-                        {' '}
-                        <UiText variants={['detail']}>
-                          <XuiTokenName address={getDdoTokenAddress(asset)}/>
+            {
+              assets.length ?
+                assets
+                  .map(asset => ({ asset, metadata: asset.findServiceByType('metadata').attributes }))
+                  .map(data => ({ ...data, defi: getDefiInfo(data.metadata) }))
+                  .map(({ asset, metadata, defi }) => (
+                    <div key={asset.id} className={b('table-wrapper')}>
+                      <UiLayout className={b('asset')}>
+                        <div className={b('checkbox')}>
+                          <img onClick={() => removeFromBasket([asset.id])} src={'assets/close.svg'} width="20px" />
+                        </div>
+                        <Link href={`/asset/${asset.id}`}>
+                          <UiText className={`pointer ${b('asset-title')}`} wrapper="h4" type="h4">{metadata.main.name}</UiText>
+                        </Link>
+                        <UiText className={b('asset-date')} type="small" variants={['detail']}>
+                          {toDate(metadata.main.dateCreated).replace(/\//g, '.')}
                         </UiText>
-                      </UiText>
-                    </UiLayout>
-                  </UiLayout>
-                </div>
-              ))
+                        <UiDivider flex />
+                        {defi?.category && defi?.network && (
+                          <>
+                            <UiLayout className={b('info')}>
+                              <UiIcon className={b('info-icon')} icon="folder" color="secondary" />
+                              <UiText variants={['secondary']}>{defi.category}</UiText>
+                              <UiText variants={['detail']}>&nbsp;&ndash;&nbsp;</UiText>
+                              <UiText variants={['secondary']}>{defi.subcategory}</UiText>
+                            </UiLayout>
+                          </>
+                        )}
+                        <UiLayout className={b('info')}>
+                          <UiIcon className={b('info-icon')} icon="tag" color="secondary" />
+                          <UiText variants={['secondary']}>
+                            <XuiTokenPrice>{metadata.main.price}</XuiTokenPrice>
+                            {' '}
+                            <UiText variants={['detail']}>
+                              <XuiTokenName address={getDdoTokenAddress(asset)} />
+                            </UiText>
+                          </UiText>
+                        </UiLayout>
+                      </UiLayout>
+                    </div>
+                  ))
+                :
+                <UiLayout type='grid' justify='center' >
+                  No item in your basket
+                </UiLayout>
+
             }
+
           </div>
           <div className={b('order-summary')}>
             <UiText type="h3" wrapper="h2">Order summary</UiText>
-            <hr style={{ borderBottom: '0', borderTop: '1px solid #8AAABE', margin: '22px 0 16px', width: '426px' }}/>
+            <hr style={{ borderBottom: '0', borderTop: '1px solid #8AAABE', margin: '22px 0 16px', width: '426px' }} />
             <UiLayout justify="end"><div className={b('items-selected')}>{basket.length} items</div></UiLayout>
-            <UiDivider/>
+            <UiDivider />
             <UiLayout justify="space-between">
               <UiText type="h4-caps" wrapper="h2">TOTAL</UiText>
               <div className={b('total-price')}><XuiTokenPrice>{totalPrice}</XuiTokenPrice></div>
             </UiLayout>
-            <UiDivider/>
-            <XuiCreateBundle assets={assets}>
-            { assets.length > 0 ? <UiButton cover>Purchase</UiButton> : <></>}
-            </XuiCreateBundle>
-            <UiDivider/>
+            <UiDivider />
+            {
+              !(isLogged && account) ?
+                <UiButton cover onClick={loginMetamask}>Connect Wallet</UiButton>
+                :
+                (network === correctNetworkName && assets.length > 0 && connected) ?
+                  <XuiCreateBundle assets={assets}>
+                    {assets.length > 0 ? <UiButton cover>Purchase</UiButton> : <></>}
+                  </XuiCreateBundle>
+                  :
+                  assets.length > 0 ? <UiButton cover onClick={handleChangeNetwork}>Switch Network & Purchase</UiButton> : <></>
+            }
+            <UiDivider />
             <UiButton cover type="alt " onClick={() => Router.push('/list')}>Back To Marketplace</UiButton>
           </div>
         </UiLayout>
