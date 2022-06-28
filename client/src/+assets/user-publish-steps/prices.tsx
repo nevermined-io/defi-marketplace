a5cde74abec91df0c53a4c863f46857025f6c1a2
josepablofm78 <jp@keyko.io>
2022-06-28 08:15:30 +0200

    Revert "removed commented code"
    This reverts commit 0180dec83154807be24636eff1e97cb00881e11e.

--- client/src/+assets/user-publish-steps/prices.tsx
+++ client/src/+assets/user-publish-steps/prices.tsx
@@ -26,6 +26,16 @@ export const PricesStep = (props: PricesProps) => {
         e.preventDefault();
         prevStep();
       }
+      /*
+<div className={b('profile-submit-container')}>
+                        <div className={b('profile-submit-container', ['updated-message'])}>
+                            {(isUpdated) ? <UiText type="h3" wrapper="h3" variants={['success']}>{successMessage}</UiText> : null}
+                        </div>
+                        <div className={b('profile-submit-container', ['submit'])}>
+                            <UiButton onClick={onSubmitUserProfile}>Update Profile</UiButton>
+                        </div>
+                    </div>
+      */
 
     return (
      
