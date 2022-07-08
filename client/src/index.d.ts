cc4ab367f91e2f8bf27f10a64ad254599486bdcb
spieljs <spieljs@gmail.com>
2022-07-08 15:45:43 +0200

    remove hardhat dependency

--- client/src/index.d.ts
+++ client/src/index.d.ts
@@ -1,9 +1,8 @@
-import { Ethereumish } from './utils/utils.interfaces'
 import Web3 from 'web3'
 
 declare global {
   interface Window {
-    ethereum: Ethereumish
+    ethereum: any
     web3: Web3
   }
 }
