c4f0356753439053144bd92cfb4a76f4474b2a4c
Iurii Tankievych <iurii.tankievych@outlook.com>
2022-09-15 14:03:46 +0100

    add a webpack plugin, disregard eslint rule

--- client/next.config.js
+++ client/next.config.js
@@ -1,31 +1,54 @@
 /** @type {import('next').NextConfig} */
 
-const gatewayUrl = process.env.NEXT_PUBLIC_GATEWAY_URI || "http://localhost:8030"
-const faucetUrl = process.env.NEXT_PUBLIC_FAUCET_URI || "http://localhost:3001"
+const gatewayUrl = process.env.NEXT_PUBLIC_GATEWAY_URI || 'http://localhost:8030'
+const faucetUrl = process.env.NEXT_PUBLIC_FAUCET_URI || 'http://localhost:3001'
 
 module.exports = {
   reactStrictMode: true,
   webpack: (config, { isServer, webpack }) => {
     // Fixes npm packages that depend on `fs` module
     if (!isServer) {
-      config.resolve.fallback.fs = false;
+      config.resolve.fallback.fs = false
     }
     config.plugins.push(
-			new webpack.IgnorePlugin({
-				resourceRegExp: /^electron$/,
-			})
-		);
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^electron$/
+      })
+    )
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: /\.tsx?$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            svgoConfig: {
+              plugins: [
+                {
+                  name: 'preset-default',
+                  params: {
+                    overrides: {
+                      removeViewBox: false
+                    }
+                  }
+                }
+              ]
+            }
+          }
+        }
+      ]
+    })
     return config
   },
   eslint: {
-    ignoreDuringBuilds: false,
+    ignoreDuringBuilds: false
   },
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
     // your project has type errors.
     // !! WARN !!
-    ignoreBuildErrors: false,
+    ignoreBuildErrors: false
   },
   publicRuntimeConfig: {
     staticFolder: '/public'
@@ -34,12 +57,12 @@ module.exports = {
     return [
       {
         source: '/api/gateway/:path*',
-        destination: `${gatewayUrl}/:path*`,
+        destination: `${gatewayUrl}/:path*`
       },
       {
         source: '/api/faucet/:path*',
-        destination: `${faucetUrl}/:path*`,
-      },
+        destination: `${faucetUrl}/:path*`
+      }
     ]
-  },
+  }
 }
