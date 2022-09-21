b96118df77e260e9f25d73c5fbf8a9d8972b42ac
Iurii Tankievych <iurii.tankievych@outlook.com>
2022-09-21 16:57:21 +0100

    modify context

--- client/src/components/+assets-query/filter-dropdown/filter-dropdown.tsx
+++ client/src/components/+assets-query/filter-dropdown/filter-dropdown.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { BEM, UiLayout } from '@nevermined-io/styles'
 import { XuiDatePicker } from './date-picker'
 import styles from './filter-dropdown.module.scss'
 import { XuiNetworkSelector } from './network-selector'
 import { XuiTypeSelector } from './type-selector'
 import { XuiSubscriptionSelector } from './subscription-selector'
+import { User } from 'src/context'
 
 const b = BEM('filter-dropdown', styles)
 
 export function XuiFilterDropdown() {
+  const { clearDropdownFilters, applyDropdownFilters } = useContext(User)
+
   return (
     <UiLayout type="grid" className={b('wrapper')}>
       <div className={b('controls')}>
@@ -18,8 +21,12 @@ export function XuiFilterDropdown() {
         <XuiDatePicker />
       </div>
       <div className={b('buttons')}>
-        <button className={b('button', ['clear'])}>Clear</button>
-        <button className={b('button', ['apply'])}>Apply Filters</button>
+        <button className={b('button', ['clear'])} onClick={clearDropdownFilters}>
+          Clear
+        </button>
+        <button className={b('button', ['apply'])} onClick={applyDropdownFilters}>
+          Apply Filters
+        </button>
       </div>
     </UiLayout>
   )
