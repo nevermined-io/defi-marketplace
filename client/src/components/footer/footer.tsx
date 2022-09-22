48a50a44efa78bdae1c7728420e57d48ddf14aac
Iurii Tankievych <iurii.tankievych@outlook.com>
2022-09-22 14:16:19 +0100

    header, footer

--- client/src/components/footer/footer.tsx
+++ client/src/components/footer/footer.tsx
@@ -9,45 +9,63 @@ const b = BEM('footer', styles)
 export function UiFooter() {
   return (
     <footer className={b()}>
-      <UiDivider type="l" />
-      <UiLayout type="sides" align="center" className={b('content')}>
-        <UiLayout>
+      <UiLayout type="container">
+        <UiDivider type="xl" />
+        <UiLayout className={b('link-row')}>
           <a href="https://www.nevermined.io/" target="_blank" rel="noopener noreferrer">
             <Image width="39" height="24" className={b('logo')} src="/assets/nevermined.svg" />
           </a>
-          <UiDivider vertical type="l" />
-          <a href="https://www.keyko.io/" target="_blank" rel="noopener noreferrer">
-            <Image width="27" height="27" className={b('logo')} src="/assets/keyko.svg" />
-          </a>
         </UiLayout>
-        <UiLayout>
+        <UiDivider type="l" />
+        <UiLayout className={b('link-row')}>
           <Link href="/about">
-            <UiText className="pointer" type="link-caps" variants={['detail']}>
-              About
-            </UiText>
+            <a>
+              <UiText className="pointer" type="link-caps" variants={['detail']}>
+                About
+              </UiText>
+            </a>
           </Link>
           <UiDivider vertical type="l" />
           <Link href="/status">
-            <UiText className="pointer" type="link-caps" variants={['detail']}>
-              Status
-            </UiText>
+            <a>
+              <UiText className="pointer" type="link-caps" variants={['detail']}>
+                Status
+              </UiText>
+            </a>
           </Link>
           <UiDivider vertical type="l" />
+          <Link href="/docs">
+            <a>
+              <UiText className="pointer" type="link-caps" variants={['detail']}>
+                Docs
+              </UiText>
+            </a>
+          </Link>
+        </UiLayout>
+      </UiLayout>
+      <UiDivider type="l" />
+      <UiLayout type="sides" align="center" className={b('content', ['secondary'])}>
+        <UiText type="small" variants={['detail']} className={b('link-column')}>
+          Keyko DeFi Marketplace {new Date().getUTCFullYear()} • All rights reserved
+        </UiText>
+        <UiLayout className={b('link-column', ['middle'])}>
           <a href="https://discord.gg/uyCT8ZmJNj" target="_blank" rel="noopener noreferrer">
             <Image width="27" height="27" className={b('logo')} src="/assets/discord.svg" />
           </a>
+          <UiDivider vertical type="l" />
+          <a href="https://www.keyko.io/" target="_blank" rel="noopener noreferrer">
+            <Image width="27" height="27" className={b('logo')} src="/assets/github.svg" />
+          </a>
         </UiLayout>
-      </UiLayout>
-      <UiDivider type="xxl" />
-      <UiLayout type="sides" align="center" className={b('content', ['secondary'])}>
-        <UiText type="small" variants={['detail']}>
-          Nevermined DeFi Marketplace {new Date().getUTCFullYear()} • All rights reserved
-        </UiText>
-        <Link href="/terms">
-          <UiText type="small" variants={['detail']}>
-            Terms & Conditions
-          </UiText>
-        </Link>
+        <UiLayout className={b('link-column', ['last'])}>
+          <Link href="/terms">
+            <a>
+              <UiText type="small" variants={['detail']}>
+                Terms & Conditions
+              </UiText>
+            </a>
+          </Link>
+        </UiLayout>
       </UiLayout>
     </footer>
   )
